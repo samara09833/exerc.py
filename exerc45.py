a52aa1e9218b0534e61b9ba46b2edef9af309049
samara09833 <machadosamara335@gmail.com>
2024-09-24 19:50:59 -0300

    exerc.py
    exerc do 1 ao 69

---
+++ exerc45.py
@@ -0,0 +1,6 @@
+# Solicita ao usuário que informe seu nome
+nome = input("Digite seu nome: ")
+
+# Loop para imprimir cada caractere do nome em uma nova linha
+for caractere in nome:
+    print(caractere)
